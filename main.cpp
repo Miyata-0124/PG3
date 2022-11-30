a80e8fd66f9a88d17f8d01f6d8d522f8f1fdbe73
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-11-30 09:50:08 +0900

    評価課題用

--- main.cpp
+++ main.cpp
@@ -6,15 +6,5 @@ using namespace std;
 
 int main() {
 
-	//éŒ¾ ‰w–¼—p•Ï”
-	//‰w–¼’Ç‰Á
-	list<char> station{ 'a','b' };
-	//list<int>::iterator itr;
-	auto itr = station.begin();
-	cout << *itr << "\n";
-	itr++;
-	cout << *itr << "\n";
-
-	system("pause");
-	return 0;
+	
 }
