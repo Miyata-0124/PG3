cba5e153332d937e1a7fe5b9e020722c5165948a
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-11-30 09:48:55 +0900

    文字の表示

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <stdio.h>
-template <typename T>
-T Min(T a, T b) {
-	if (a >= b)
-	{
-		return static_cast<T>(b);
-	}
-	return  static_cast<T>(a);
-}
-template <>
-char Min<char>(char a, char b) {
-	printf("”š‚Í‘ã“üo—ˆ‚Ü‚¹‚ñ\n");
-	return 0;
-}
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
 
+int main() {
 
-int main(void) {
-	printf("%d\n", Min<int>(10, 5));
-	printf("%f\n", Min<float>(1.0f, 5.0f));
-	printf("%f\n", Min<double>(1.0, 5.0));
-	Min<char>('1', '5');
+	//éŒ¾ ‰w–¼—p•Ï”
+	//‰w–¼’Ç‰Á
+	list<char> station{ 'a','b' };
+	//list<int>::iterator itr;
+	auto itr = station.begin();
+	cout << *itr << "\n";
+	itr++;
+	cout << *itr << "\n";
 
+	system("pause");
 	return 0;
 }
