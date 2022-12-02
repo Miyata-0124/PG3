ed8d3f08dd0926b79c019f2dc4c4a6598bd2576d
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-12-02 19:48:36 +0900

    評価課題1_b

--- main.cpp
+++ main.cpp
@@ -4,49 +4,100 @@
 
 // 単方向リストの構造体
 typedef struct Cell {
-	char num[8];
+	char str[8];
+	struct Cell* prev;
 	struct Cell* next;
 } Cell;
 
 //データ追加の関数
-void Create(Cell *head, const char *num);
+void Create(Cell *head, const char *str);
 //一覧表示の関数
 void Index(Cell *head);
+//削除の関数
+void Delete(Cell* end);
 
 int main() {
-	char num[8];
+	char str[8];
 	// 先頭のセル
 	Cell head;
-	head.next = nullptr;																																																																																																																																																																																																																																																																																																																																																																																																																																																																																
+	head.next = nullptr;
+
+	int scene = 0;;
 	
-	while (true) {
-		printf("好きな値を入力 : ");
+	Create(&head, "banana\n");
+	Create(&head, "apple\n");
+	Create(&head, "orange\n");
 
-		scanf_s("%s", num, 8);
+	while (true) 
+	{
+		switch (scene)
+		{
+			case 0:
+			
+				printf("[要素の操作]\n");
+				printf("1.一覧表示\n");
+				printf("2.要素の挿入\n");
+				printf("3.要素の削除\n\n");
+				printf("----------------\n");
+				printf("選択したい番号を入力\n");
+				scanf_s("%d", &scene);
+				break;
+			
+			// 一覧用
+			case 1:
+			
+				printf("[要素一覧]\n");
+				Index(&head);
+				printf("----------------\n");
+				printf("0.初期に戻る\n");
+				scanf_s("%d", &scene);
 
-		Create(&head, num);
+				break;
+			
+			// 挿入用
+			case 2:
+			
+				printf("[要素の挿入]\n");
+				printf("0.初期に戻る\n");
+				scanf_s("%s\n", str, 8);
+				Create(&head, str);
+				printf("%sを最後尾に挿入\n", str);
+				printf("----------------\n");
+				printf("0.初期に戻る\n");
+				scanf_s("%d", &scene);
 
-		Index(&head);
+				break;
+			
+			// 削除用
+			case 3:
+			
+				Delete(&head);
+				printf("----------------\n");
+				printf("0.初期に戻る\n");
+				scanf_s("%d", &scene);
+				break;
+		}
 	}
 	system("pause");
 	return 0;
 }
 
 //データ追加の関数
-void Create(Cell* head, const char *num)
+void Create(Cell* head, const char *str)
 {
 	// 新規作成
 	Cell *createCell;
 	// メモリ確保
 	createCell = (Cell*)malloc(sizeof(Cell));
 
-	strcpy_s(createCell->num, 8, num);
+	strcpy_s(createCell->str, 8, str);
 
 	createCell->next = nullptr;
 
 	// 最後尾を検索
-	while (head->next!=nullptr){
+	while (head->next != nullptr){
 		head = head->next;
+		createCell->prev = head;
 	}
 
 	// 最後尾にポインタを代入
@@ -56,11 +107,24 @@ void Create(Cell* head, const char *num)
 void Index(Cell* head)
 {
 	// 表示する
+	int num = 0;
 	while (head->next != nullptr)
 	{
 		head = head->next;
-		printf("%s,", head->num);
+		printf("%s,", head->str);
+		num++;
 	}
 	// 少しずらす
 	printf("\n");
+	printf("要素数:%d\n", num);
+}
+
+void Delete(Cell* end)
+{
+	while (end->next != nullptr)
+	{
+		end = end->next;
+	}
+	end->prev->next = nullptr;
+	printf("最後尾を削除\n");
 }
